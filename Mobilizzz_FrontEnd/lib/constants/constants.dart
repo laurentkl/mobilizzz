1f480c6d854d8e2ec17addb3642f21af4021e279
Laurent Klein <laurent.kl1996@gmail.com>
2024-06-08 13:34:27 +0200

    before removing record provider

--- Mobilizzz_FrontEnd/lib/constants/constants.dart
+++ Mobilizzz_FrontEnd/lib/constants/constants.dart
@@ -1,7 +1,9 @@
 class AppConstants {
 
   // Example API URL constant
-  static const String apiUrl = 'http://10.0.10.49:5169';
+  // static const String apiUrl = 'http://10.0.10.49:5169';
+  static const String apiUrl = 'http://localhost:5169';
+  // static const String apiUrl = 'http://192.168.0.99:5169';
   // static const String apiUrl = 'http://10.0.10.55:5169';
 
 }
