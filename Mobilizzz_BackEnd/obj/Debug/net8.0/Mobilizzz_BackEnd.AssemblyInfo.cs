3b218d93e2900b76f62b81d41c0f9782764efa5a
Laurent Klein <laurent.kl1996@gmail.com>
2024-06-09 21:24:34 +0200

    gitignore for backend

--- Mobilizzz_BackEnd/obj/Debug/net8.0/Mobilizzz_BackEnd.AssemblyInfo.cs
+++ Mobilizzz_BackEnd/obj/Debug/net8.0/Mobilizzz_BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mobilizzz_BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4da26511926b47ecf1f6a09af2148404fda4170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mobilizzz_BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mobilizzz_BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
